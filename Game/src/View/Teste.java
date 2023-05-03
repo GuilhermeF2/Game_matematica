a0d6d4f53ce8aed82080b32f1ea273f5004a9207
guife <guife@DESKTOP-TK5IQ4V>
2023-05-03 13:31:45 -0300

    Commit cod

---
+++ Game/src/View/Teste.java
@@ -0,0 +1,14 @@
+package View;
+
+import Model.Calcular;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Calcular calc = new Calcular(4);
+		
+		System.out.println(calc);
+
+	}
+
+}
